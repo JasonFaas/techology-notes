42f55c647c747a4a1189d74b536176a8cf68dff8
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2025-09-23 21:45:42 -0700

    More python katas

---
+++ programming-languages/python/kata/hw.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+a = set(["a", "b"])
+for idx, val in enumerate(a):
+  print(val)
+
+
+strHW = "hello world"
+print(strHW[4])
+print(strHW.find("wa"))
+print(''.join(sorted(strHW)))
+
+arrW = ['a', 'b']
+arrW.append('c')
+del arrW[0]
+print(arrW)
+arrW.remove("b")
+print(arrW)
