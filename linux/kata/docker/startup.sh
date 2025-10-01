03c22c4b2340538cc0ffaac5face3ac3a18aba90
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2025-10-01 11:23:27 -0700

    Docker linux update

--- linux/kata/docker/startup.sh
+++ linux/kata/docker/startup.sh
@@ -1,8 +1,6 @@
-docker build -t ubuntu-systemd .
-docker run --privileged -it \
+docker build -t $1 . && docker run --privileged -it \
   --cgroupns=host \
   --tmpfs /tmp --tmpfs /run --tmpfs /run/lock \
   -v /sys/fs/cgroup:/sys/fs/cgroup \
-  ubuntu-systemd
-
+  $1
 
---
+++ linux/kata/small-tools/test_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for X in 1 2 3 4 5
+do
+  echo $X $1
+done
+
