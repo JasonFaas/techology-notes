a93f64101c5dc381a5def2308f9fe6b2549187de
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2025-10-06 21:14:46 -0700

    Python kata

---
+++ programming-languages/python/kata/what.py
@@ -0,0 +1,28 @@
+print(bool([]))
+class HW():
+  def __init__(self):
+    pass
+
+  def hw(self):
+    print("HW!")
+
+what = HW()
+what.hw()
+
+
+a = {1:2,3:4}
+# for b,c in a.items():
+#   print(b+':'+c)
+
+c = [2,3,5,7,11,13,15]
+print(c)
+c.append(19)
+print(c)
+c.pop()
+c.pop()
+print(c)
+del c[3]
+print(c)
+c.remove(2)
+print(c)
+
